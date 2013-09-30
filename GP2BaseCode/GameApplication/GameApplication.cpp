3c128572ce158ac07dd6d1388047cb0d9ce65b42
smcvei200 <stevo86_mcv@live.co.uk>
2013-09-30 11:15:40 +0100

    Source code been added

---
+++ GP2BaseCode/GameApplication/GameApplication.cpp
@@ -0,0 +1,117 @@
+#include "GameApplication.h"
+
+//should really check to see if we are on a windows platform
+#include "../Window/Win32Window.h"
+
+//Constructor
+CGameApplication::CGameApplication(void)
+{
+	//Set to NULL
+	m_pWindow = NULL;
+	
+	//Set the window name to GP2
+	m_GameOptionDesc.gameName=TEXT("GP2");
+
+	//Window height and width
+	m_GameOptionDesc.width = 640;
+	m_GameOptionDesc.height = 480;
+
+	//Full screen
+	m_GameOptionDesc.fullscreen = false;
+
+	//Config Options
+	m_ConfigFileName = TEXT("game.cfg");
+
+}
+
+//Deconstructor
+CGameApplication::~CGameApplication(void)
+{
+	if(m_pWindow)
+	{
+		delete m_pWindow;
+		m_pWindow = NULL;
+	}
+}
+
+//Init
+//This initialises all subsystems
+bool CGameApplication::init()
+{
+	if(!parseConfigFile())
+		return false;
+	if(!initWindow())
+		return false;
+	if(!initGraphics())
+		return false;
+	if(!initInput())
+		return false;
+	if(!initGame())
+		return false;
+	return true;
+}
+
+//called to parse the config file
+bool CGameApplication::parseConfigFile()
+{
+	return true;
+}
+
+//initInput - initialises the Input
+bool CGameApplication::initInput()
+{
+	return true;
+}
+
+//initPhysics - initialises the Physics
+bool CGameApplication::initPhysics()
+{
+	return true;
+}
+
+//initGraphics- initialises the graphics subsystem
+bool CGameApplication::initGraphics()
+{
+	return true;
+
+}
+
+//initWindow - initialises the window
+bool CGameApplication::initWindow()
+{
+	//Create a Win32Window
+	m_pWindow = new CWin32Window();
+	m_pWindow->init(m_GameOptionDesc.gameName, m_GameOptionDesc.width, m_GameOptionDesc.height, m_GameOptionDesc.fullscreen);
+	return true;
+}
+
+//called to init the game elements
+bool CGameApplication:: initGame()
+{
+	return true;
+}
+
+//Called t6o put the game in a loop(aka game loop)
+void CGameApplication::run()
+{
+	//while window is not closed
+	while(m_pWindow->running())
+		{
+			//check for all windows messages
+			m_pWindow->checkForWindowMessages();
+			//update
+			update();
+			//render
+			render();
+	}
+}
+
+//Render, called to draw one frame of the game
+void CGameApplication::render()
+{
+}
+
+//Update, called to update the game
+void CGameApplication::update()
+{
+}
